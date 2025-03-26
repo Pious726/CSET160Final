094425322abe4112c4402fbef64d633bda9e0771
Tyler M. <tjmcdowell726@gmail.com>
2025-03-26 08:17:57 -0400

    Created files and added base flask code and html skeleton
    Co-authored-by: ryansweeney157 <ryansweeney157@users.noreply.github.com>

---
+++ main.py
@@ -0,0 +1,11 @@
+from flask import Flask, render_template, request
+from sqlalchemy import create_engine, text
+
+app = Flask(__name__)
+conn_str = "mysql://root:cset155@localhost/exams"
+engine = create_engine(conn_str, echo=True)
+conn = engine.connect()
+
+@app.route('/')
+def hello():
+    return render_template('base.html')
