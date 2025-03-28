54c7aeb608fc8212c71a298f56ca6152e5c054b6
Tyler M. <tjmcdowell726@gmail.com>
2025-03-28 11:05:53 -0400

    added IsLoggedIn column to more accurately define who is logged in

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, flash
 from sqlalchemy import create_engine, text
-
+ 
 app = Flask(__name__)
 conn_str = "mysql://root:cset155@localhost/exams"
 engine = create_engine(conn_str, echo=True)
 conn = engine.connect()
+app.secret_key = 'lunchFarts'
 
 @app.route('/', methods=["GET"])
 def getAccounts():
@@ -13,7 +14,7 @@ def getAccounts():
 @app.route('/', methods=["POST"])
 def signup():
     try:
-        conn.execute(text('insert into accounts(Username, EmailAddress, UserPassword, AccountType) values(:Username, :EmailAddress, :UserPassword, :AccountType)'), request.form)
+        conn.execute(text('insert into accounts(Username, EmailAddress, UserPassword, AccountType, IsLoggedIn) values(:Username, :EmailAddress, :UserPassword, :AccountType, 0)'), request.form)
 
         account_type = request.form.get("AccountType")
         account_id = conn.execute(text('select last_insert_id()')).scalar()
@@ -30,16 +31,23 @@ def signup():
 
 @app.route('/login.html', methods=["GET"])
 def getlogins():
+    conn.execute(text('update accounts set IsLoggedIn = 0'))
+    conn.commit()
     return render_template('login.html')
 
 @app.route('/login.html', methods=["POST"])
 def login():
     try:
         username = request.form.get("Username")
+        print(username)
         password = request.form.get("UserPassword")
+        print(password)
         query = conn.execute(text(f'select UserPassword from accounts where Username = :username'), {'username': username}).scalar()
+        print(query)
         
         if query and password == query:
+            conn.execute(text('update accounts set IsLoggedIn = 1 where Username = :username'),  {'username': username})
+            conn.commit()
             return render_template('home.html', error = None, success = "Successful")
     except:
         return render_template('login.html', error = "Incorrect Username or Password", success = None)
@@ -59,38 +67,15 @@ def accounts():
     accounts = conn.execute(text(query), {"AccountType": account_type}).all() if account_type else conn.execute(text(query)).all()
     return render_template('accounts.html', accounts = accounts)
 
-
-@app.route("/tests.html", methods=["POST"])
-def create_tests():
+@app.route("/tests.html", methods=["POST", "GET"])
+def create_test():
     try:
-        TestName = request.form.get('TestName', '').strip()
-        questions = [request.form.get(f'question{i}', '').strip() for i in range(1, int(request.form.get('question_count', '1')) + 1) if request.form.get(f'question{i}')]
-    
-        if TestName and questions:
-            with engine.connect() as conn:
-                conn.execute("insert into tests (TestName) values (:TestName)", {"TestName": TestName})
-                TestID = conn.execute("select last_insert_id()").scalar()
-                conn.execute(
-                     "insert into questions (TestID, question_text) values (:TestID, :question_text)",
-                    [{"TestID": TestID, "question_text": q} for q in questions]
-
-                )
-                conn.commit()
-            return redirect("/tests.html")
+        conn.execute(text('Insert into tests(TestName, Question) values(:TestName, :Question)'), request.form)
+        return render_template('tests.html', error = None, success = "Successful")
     except:
-        return render_template("tests.html", error="Submission failed")
+        return render_template('tests.html', error = "failed", succes = None)
 
-@app.route("/tests.html", methods=["GET"])
-def get_tests():
-    with engine.connect() as conn:
-        result = conn.execute("select TestID, TestName from tests")
-        tests = result.fetchall()
 
-        for test in tests:
-            questions_result = conn.execute("Select question-text from questions where TestID = :TestID", {"TestID", test["id"]})
-            test["questions"] = [row["question_text"] for row in questions_result.fetchall()]
-
-    return render_template('tests.html', question_count=1, questions=[], TestName="", submitted_tests=tests)
 
 if __name__ == '__main__':
     app.run(debug=True)
